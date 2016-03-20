5119f9b0764aaf8da5e632a0cdacc4818b69069e
Kirill Stytsenko <kirill@styts.com>
2016-03-20 13:11:47 +0100

    Save crontab

--- bin/backup_crontab.sh
+++ bin/backup_crontab.sh
@@ -1,6 +1,5 @@
 filename="crontab.$(hostname)"
 crontab -l > ~/.dotfiles/$filename
 cd ~/.dotfiles
-git add $filename
-git commit -m "crontab backup created"
+git commit -m "crontab backup created" -- $filename
 cd -
